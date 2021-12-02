3feaedf68e2bfb36e194e1793fee12300a7794ae
snowMan108 <snowmanhi108@gmail.com>
2021-12-02 18:04:01 +0300

    changed embed youtobe

--- src/views/Tutorials/index.tsx
+++ src/views/Tutorials/index.tsx
@@ -1,22 +1,20 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import { Flex, Heading } from '@twinkykms/rubyswap-uikit'
 import Page from 'components/Layout/Page'
 import PageHeader from 'components/PageHeader'
 import { useTranslation } from 'contexts/Localization'
 
-import { AppHeader, AppBody } from '../../components/App'
-
 const EmbedVideo = styled.iframe`
-  width: 420px;
-  height: 340px;
+  width: 520px;
+  height: 420px;
   border-radius: 12px;
 `
 const VideoWrapper = styled.div`
   display: flex;
   width: 100%;
   flex-wrap: wrap;
-  gap: 24px;
+  gap: 42px;
   justify-content: center;
 `
 
@@ -38,10 +36,34 @@ export default function Tutorials() {
       </PageHeader>
       <Page>
         <VideoWrapper>
-          <EmbedVideo className="tutor-video" src="https://youtu.be/Fiy7YDaXUz4" />
-          <EmbedVideo className="tutor-video" src="https://youtu.be/YGE-GxArA-M" />
-          <EmbedVideo className="tutor-video" src="https://youtu.be/LBGhu2G6_m4" />
-          <EmbedVideo className="tutor-video" src="https://youtu.be/8JOHkP3XntA" />
+          <EmbedVideo
+            src={`https://www.youtube.com/embed/Fiy7YDaXUz4`}
+            frameBorder="0"
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="How to SWAP RUBY"
+          />
+          <EmbedVideo
+            src={`https://www.youtube.com/embed/YGE-GxArA-M`}
+            frameBorder="0"
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="How to Add liquidity"
+          />
+          <EmbedVideo
+            src={`https://www.youtube.com/embed/LBGhu2G6_m4`}
+            frameBorder="0"
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="how to FARM RUBY"
+          />
+          <EmbedVideo
+            src={`https://www.youtube.com/embed/8JOHkP3XntA`}
+            frameBorder="0"
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="How to STAKE RUBY"
+          />
         </VideoWrapper>
       </Page>
     </>
